f7579fc2e12431f8aacafe23d8d5ce5e8bcf3e9b
Nakamura-PC <Nakamura-PC@DESKTOP-O3CVL0Q.senacsp.edu.br>
2017-10-10 17:57:10 -0300

    Inicio integração do Tema com Servlet.

---
+++ PI3_ProjetoLeonsDev/src/main/webapp/js/html5.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.createElement("article");
+document.createElement("aside");
+document.createElement("audio");
+document.createElement("canvas");
+document.createElement("command");
+document.createElement("datalist");
+document.createElement("details");
+document.createElement("embed");
+document.createElement("figcaption");
+document.createElement("figure");
+document.createElement("footer");
+document.createElement("header");
+document.createElement("hgroup");
+document.createElement("keygen");
+document.createElement("mark");
+document.createElement("meter");
+document.createElement("nav");
+document.createElement("output");
+document.createElement("progress");
+document.createElement("rp");
+document.createElement("rt");
+document.createElement("ruby");
+document.createElement("section");
+document.createElement("source");
+document.createElement("summary");
+document.createElement("time");
+document.createElement("video");
