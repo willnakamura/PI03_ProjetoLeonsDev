059799dfbf4225aa6ad86d923b82627fbda591a3
Josué <Josu@DESKTOP-RR5BE7B>
2017-11-13 20:01:16 -0200

    Finalizado DAO de relatório, objeto de relatório e proc

--- PI3_ProjetoLeonsDev/src/main/java/br/senac/tads3/pi3b/leonsdev/DAOs/Relatorios.java
+++ PI3_ProjetoLeonsDev/src/main/java/br/senac/tads3/pi3b/leonsdev/DAOs/Relatorios.java
@@ -6,6 +6,7 @@
 package br.senac.tads3.pi3b.leonsdev.DAOs;
 
 import br.senac.tads3.pi3b.leonsdev.dbUtils.ConnectionUtils;
+import br.senac.tads3.pi3b.leonsdev.relatorios.RelatorioClientes;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,7 +21,7 @@ import br.senac.tads3.pi3b.leonsdev.relatorios.RelatorioReservas;
  *
  * @author Josué
  */
-public class ReportVendas {
+public class Relatorios {
     public static List<RelatorioReservas> listarVendas(Date dataInicio, Date dataFim)
             throws SQLException, Exception {
 
@@ -95,6 +96,81 @@ public class ReportVendas {
         }
         //Retorna a lista do banco de dados
         return reservas;
+
+    }
+ public static List<RelatorioClientes> listarClientes(Date dataInicio, Date dataFim)
+            throws SQLException, Exception {
+
+        //Monta a string de listagem de vendas no banco, considerando
+        //apenas as colunas usadas no relatório
+        String sql = "CALL ReportClientes (?, ?)";
+        /*
+        String sql = "SELECT C.NOME nome, P.CATEGORIA categoria, P.DESCRICAO descricao, VP.QTD qtd, (VP.qtd*P.preco) total, VP.DATA data "
+                + "FROM cliente C "
+                + "INNER JOIN venda V ON V.CLIENTE_ID = C.CLIENTE_ID "
+                + "INNER JOIN vendprod VP ON VP.VENDA_ID = V.VENDA_ID "
+                + "INNER JOIN produto P ON P.PRODUTO_ID = VP.PRODUTO_ID "
+                + "WHERE VP.DATA BETWEEN ? AND ?";*/
+        //Lista de produtos de resultado
+        List<RelatorioClientes> clientes = null;
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        //Armazenará os resultados do banco de dados
+        ResultSet result = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um preparedStatement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do PreparedStatement
+            
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd 00:00:00.000");
+            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd 00:00:00.000");
+            preparedStatement.setString(1, sdf.format(dataInicio));
+            preparedStatement.setString(2, sdf1.format(dataFim));
+
+            //Executa a consulta SQL no banco de dados
+            result = preparedStatement.executeQuery();
+            //Itera por cada item do resultado
+            while (result.next()) {
+                //Se a lista não foi inicializada, a inicializa
+                if (clientes == null) {
+                    clientes = new ArrayList<>();
+                }
+                //Cria uma instância de Venda e popula com os valores do BD
+                RelatorioClientes clientela = new RelatorioClientes();
+                
+                clientela.setDataCadastro(result.getDate("DataCadastro"));
+                clientela.setNome(result.getString("Nome"));
+                clientela.setSobreNome(result.getString("Sobrenome"));
+                clientela.setCPF(result.getString("CPF"));
+                clientela.setEmail(result.getString("Email"));
+                clientela.setNível(result.getString("Nivel"));
+                clientela.setMilhasTotal(result.getInt("MilhasTotal"));
+                clientela.setQtdReservas(result.getInt("TotalReservas"));
+                clientela.setDataUltimaCompra(result.getDate("UltimaReserva"));
+                //Adiciona a instância na lista
+                clientes.add(clientela);
+            }
+        } finally {
+            //Se o result ainda estiver aberto, realiza seu fechamento
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            //Se o preparedStatement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        //Retorna a lista do banco de dados
+        return clientes;
 
     }
 
