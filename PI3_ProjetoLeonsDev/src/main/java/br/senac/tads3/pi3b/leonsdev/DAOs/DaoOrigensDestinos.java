e83d5646506052f217dfb8a2adc6eb57f1c09dbf
Josué <Josu@DESKTOP-RR5BE7B>
2017-11-15 10:39:04 -0200

    Criado Dao para consultar as origens e destinos

---
+++ PI3_ProjetoLeonsDev/src/main/java/br/senac/tads3/pi3b/leonsdev/DAOs/DaoOrigensDestinos.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3b.leonsdev.DAOs;
+
+import br.senac.tads3.pi3b.leonsdev.dbUtils.ConnectionUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Josué
+ */
+public class DaoOrigensDestinos {
+    public static ArrayList OrigemDestino = new ArrayList();
+
+    public static ArrayList OrigensDestinos()
+            throws SQLException, Exception {
+        String sql = "call BuscaVoos()";
+
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        //Armazenará os resultados do banco de dados
+        ResultSet result = null;
+        try {
+            
+//                           String dataString = ("2018-09-02");
+//DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+//java.sql.Date data = new java.sql.Date(fmt.parse(dataString).getTime());
+            
+            
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+
+            //Executa a consulta SQL no banco de dados
+            result = preparedStatement.executeQuery();
+
+            //Itera por cada item do resultado
+            while (result.next()) {
+                //Se a lista não foi inicializada, a inicializa
+                OrigemDestino.add(result.getString("Origem"));
+                OrigemDestino.add(result.getString("Destino"));
+            }
+
+        } finally {
+            //Se o result ainda estiver aberto, realiza seu fechamento
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        //Retorna a lista de aeronaves do banco de dados
+        return OrigemDestino;
+    }
+
+}
