5e82e1c7686801bba7da8877e2655c0a0a8fc458
Josué <Josu@DESKTOP-RR5BE7B>
2017-11-26 18:43:30 -0200

    Criação DaoAssentos

---
+++ PI3_ProjetoLeonsDev/src/main/java/br/senac/tads3/pi3b/leonsdev/DAOs/DaoAssentos.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3b.leonsdev.DAOs;
+
+import br.senac.tads3.pi3b.leonsdev.dbUtils.ConnectionUtils;
+import br.senac.tads3.pi3b.leonsdev.voos.classes.Assento;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Josué
+ */
+public class DaoAssentos {
+    public static List<Assento> listar(Date dataVoo, int nrVoo)
+            throws SQLException, Exception {
+        String sql = "CALL AssentosDisponiveis (dataVoo=?, NrVoo=?)";
+
+        List<Assento> listaAssentos = null;
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        //Armazenará os resultados do banco de dados
+        ResultSet result = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setDate(1, dataVoo);
+            preparedStatement.setInt(2, nrVoo);
+
+            //Executa a consulta SQL no banco de dados
+            result = preparedStatement.executeQuery();
+
+            //Itera por cada item do resultado
+            while (result.next()) {
+                //Se a lista não foi inicializada, a inicializa
+                if (listaAssentos == null) {
+                    listaAssentos = new ArrayList<>();
+                }
+
+                Assento assento = new Assento();
+                assento.setPreco(result.getDouble("Preco"));
+                assento.setnAssento(result.getString("Assento"));
+               
+
+                //Adiciona a instância na lista
+                listaAssentos.add(assento);
+            }
+        } finally {
+            //Se o result ainda estiver aberto, realiza seu fechamento
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        //Retorna a lista de clientes do banco de dados
+        return listaAssentos;
+    }
+}
